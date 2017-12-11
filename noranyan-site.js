94a8fa47da638c003b78dbcf7413ae662b94362d
katsuhirokiyomoto <katsuhiro.k@hotmail.com>
2017-12-11 13:39:56 +0000

    complete noranyan-site

--- noranyan-site.js
+++ noranyan-site.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel(
         {
             loop: true,
-            nav: false,
+            nav: true,
             dots: true,
             items: 1,
             autoplay: true
